839745a26f883f47fd7cb3501a638fed9054cb66
Ofri Haviv <haviv.ofri@gmail.com>
2023-02-04 00:27:42 +0200

    Added FourierRenderer struct.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use num::{
     complex::{Complex64, ComplexFloat},
     Complex,
 };
+use renderer::FourierRenderer;
 use sfml::{
     graphics::{Color, PrimitiveType, RenderStates, RenderTarget, Vertex, View},
     system::{Vector2, Vector2f},
@@ -18,20 +19,20 @@ pub mod function;
 pub mod renderer;
 
 fn target(x: f64) -> Complex<f64> {
-    // Complex::new(
-    //     if x > 2.0/3.0 { 1.0 } else { -1.0 },
-    //     if x > 1.0/3.0 { 1.0 } else { -1.0 },
-    // )
-    let x = PI*x*3.5;
-
     Complex::new(
-        if x.cos() < 0.0{ -1.0}else {1.0},
-        if x.sin() < 0.0{ -1.0}else {1.0},
+        if x > 2.0/3.0 { 1.0 } else { -1.0 },
+        if x > 1.0/3.0 { 1.0 } else { -1.0 },
     )
+    // let x = PI*x*3.5;
+
+    // Complex::new(
+    //     if x.cos() < 0.0{ -1.0}else {1.0},
+    //     if x.sin() < 0.0{ -1.0}else {1.0},
+    // )
 
     // Complex64::new(
-    //     x.cos(),
-    //     x.sin()
+        // x.cos(),
+        // x.sin()
     // )
 }
 
@@ -47,6 +48,10 @@ fn main() {
     let size = Vector2 { x: 1.0, y: 1.0 };
     // println!("{}",(|x:f64|Complex::new(0.0,x*-2.0*PI).exp()).integrate(StepRange::new(0.0,1.0,0.00001)));
     let f = FourierTransform::new(-1000..1001,target, &args);
+
+    let renderer = FourierRenderer::new(f);
+
+    renderer.run();
 
 
     
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,30 +1,44 @@
-use std::{
-    collections::{HashMap, LinkedList},
-    iter::Map, time::Instant, f64::consts::PI,
+use std::{collections::VecDeque, f64::consts::PI, time::Instant};
+
+use num::{
+    complex::{Complex64, ComplexFloat},
+    Complex,
+};
+use sfml::{
+    graphics::{Color, PrimitiveType, RenderStates, RenderTarget, RenderWindow, Vertex, View},
+    system::{Vector2, Vector2f},
+    window::{Event, Style},
 };
 
-use num::{complex::Complex64, Complex};
-use sfml::{graphics::{RenderWindow, RenderTarget, Color, PrimitiveType, RenderStates, View, Vertex}, window::{Style, Event}, system::{Vector2f, Vector2}};
+use crate::{fourier_transform::FourierTransform, function::Function};
 
-use crate::fourier_transform::FourierTransform;
-
+#[derive(Default)]
 struct TransformRenderState {
-    pub last: LinkedList<Complex64>,
+    pub last: Vec<Vertex>,
 }
 
-struct Renderer {
+impl TransformRenderState {
+    pub fn push(&mut self, c: Vertex) {
+        self.last.push(c);
+    }
+
+    pub fn clear(&mut self) {
+        self.last.clear()
+    }
+
+    pub(crate) fn as_slice(&self) -> &[Vertex] {
+        self.last.as_slice()
+    }
+}
+
+pub struct FourierRenderer {
     window: RenderWindow,
-    transforms: Vec<(FourierTransform, TransformRenderState)>,
+    transforms: FourierTransform,
 }
-
 
 fn create_window() -> RenderWindow {
-    let mut window = sfml::graphics::RenderWindow::new(
-        (800, 800),
-        "nice",
-        Style::CLOSE,
-        &Default::default(),
-    );
+    let mut window =
+        sfml::graphics::RenderWindow::new((800, 800), "nice", Style::CLOSE, &Default::default());
     window.set_framerate_limit(144);
     window.set_view(&sfml::graphics::View::new(
         Vector2f::new(0.0, 0.0),
@@ -33,61 +47,80 @@ fn create_window() -> RenderWindow {
     window
 }
 
-impl Renderer {
-    fn new() -> Self {
+impl FourierRenderer {
+    pub fn new(transforms: FourierTransform) -> Self {
         Self {
             window: create_window(),
-            transforms: Default::default()
+            transforms,
         }
-    
     }
 
+    pub fn run(mut self) {
+        let zero = Vector2f::new(0.0, 0.0);
+        let background = Color::rgb(69, 69, 69);
+        let mut start = Instant::now();
+        let mut x = 0.0;
+        let speed = 0.1;
+        let mut state = TransformRenderState::default();
 
-    fn run(&mut self)  {
+        let mut transforms = self
+            .transforms
+            .get_data()
+            .clone()
+            .into_iter()
+            .collect::<Vec<_>>();
+        transforms.sort_by(|x, y| x.1.abs().total_cmp(&y.1.abs()).reverse());
+
         let mut array = Vec::new();
-    let zero = Vector2f::new(0.0, 0.0);
-    let background = Color::rgb(69, 69, 69);
-    let mut start = Instant::now();
-    let mut x = 0.0;
-    let speed = 0.1;
 
-    while self.window.is_open() {
-        while let Some(e) = self.window.poll_event() {
-            if let Event::Closed = e {
-                self.window.close()
+        while self.window.is_open() {
+            while let Some(e) = self.window.poll_event() {
+                if let Event::Closed = e {
+                    self.window.close()
+                }
             }
-        }
-        
-        let mut vec = f.get_data().clone().into_iter().collect::<Vec<_>>();
-        vec.sort_by(|x, y| x.1.abs().total_cmp(&y.1.abs()).reverse());
-        x += (Instant::now() - start).as_secs_f64() * speed;
-        start = Instant::now();
-        let mut last = Complex64::default();
-        array.push(Vertex::new(
-            Vector2::new(last.re as f32, last.im as f32),
-            Color::WHITE,
-            zero,
-        ));
-        for (pow, c) in vec.iter() {
-            last += c * Complex::new(0.0, 2.0 * PI * x * *pow as f64).exp();
+            if x>=2.0 {
+                x= 0.0;
+                state.clear();
+            }
+            x += (Instant::now() - start).as_secs_f64() * speed;
+            
+            start = Instant::now();
+            let mut last = Complex64::default();
             array.push(Vertex::new(
                 Vector2::new(last.re as f32, last.im as f32),
-                Color::rgba(255, 255, 255, 255),
+                Color::WHITE,
                 zero,
             ));
-        }
-        drop(vec);
-        let a = array.last().unwrap();
-        print!("x:{x}\r");
+            for (pow, c) in transforms.iter() {
+                last += c * Complex::new(0.0, 2.0 * PI * x * *pow as f64).exp();
+                array.push(Vertex::new(
+                    Vector2::new(last.re as f32, last.im as f32),
+                    Color::rgba(255, 255, 255, 255),
+                    zero,
+                ));
+            }
 
-        self.window.draw_primitives(
-            array.as_slice(),
-            PrimitiveType::LINE_STRIP,
-            &RenderStates::default(),
-        );
-        self.window.display();
-        self.window.clear(background);
-        array.clear();
-    }
+            let mut a = *array.last().unwrap();
+            a.color = Color::RED;
+            state.push(a);
+            
+            print!("x:{x}\r");
+
+            self.window.draw_primitives(
+                array.as_slice(),
+                PrimitiveType::LINE_STRIP,
+                &RenderStates::default(),
+            );
+            self.window.draw_primitives(
+                state.as_slice(),
+                PrimitiveType::LINE_STRIP,
+                &RenderStates::default(),
+            );
+            
+            self.window.display();
+            self.window.clear(background);
+            array.clear();
+        }
     }
 }
